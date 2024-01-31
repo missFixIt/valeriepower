736836e3eba2739458e1dab9bcab336cb1accdbd
missFixIt <126031655+missFixIt@users.noreply.github.com>
2024-01-31 09:54:51 -0800

    updates

--- scripts.js
+++ scripts.js
@@ -1,21 +1,40 @@
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
-const hamburgerIcon = document.querySelector(".fa-bars");
+const menuIcon = document.querySelector(".fa-solid");
 const closeIcon = document.querySelector(".fa-xmark");
+const seeMore = document.querySelector(".see-more");
+
 navToggle.addEventListener("click", menuFunction);
 primaryNav.addEventListener("click", menuFunction);
+seeMore.addEventListener("click", expandFunction);
+
 function menuFunction() {
   const visibility = primaryNav.getAttribute("data-visible");
-  console.log(visibility);
+  // console.log(visibility);
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", true);
     navToggle.setAttribute("aria-expanded", true);
-    hamburgerIcon.style.display = "none";
-    closeIcon.style.display = "block";
+    menuIcon.className = "fa-solid fa-xmark";
   } else {
     primaryNav.setAttribute("data-visible", false);
     navToggle.setAttribute("aria-expanded", false);
-    closeIcon.style.display = "none";
-    hamburgerIcon.style.display = "block";
+    menuIcon.className = "fa-solid fa-bars";
+    console.log(menuIcon.className);
+  }
+}
+
+function expandFunction() {
+  const expanded = seeMore.getAttribute("expanded");
+  // console.log(visibility);
+  if (expanded === "false") {
+    primaryNav.setAttribute("data-expanded", true);
+    navToggle.setAttribute("aria-expanded", true);
+    seeMore.className = "fa-solid fa-chevron-down see-more";
+  } else {
+    primaryNav.setAttribute("data-expanded", false);
+    navToggle.setAttribute("aria-expanded", false);
+    seeMore.className = "fa-solid fa-chevron-up see-more";
+    // console.log(menuIcon.className);
+    //write something here to remove the cut off from the text so it expands and takes up the whole screen with all the text and photos.
   }
 }

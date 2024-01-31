bd1199bdce3c7dfb44fd8ce99288e443b931d1dd
missFixIt <126031655+missFixIt@users.noreply.github.com>
2024-01-30 20:24:34 -0800

    fixed some font sizing issues, got mobile menu working but not perfect

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
-
+const hamburgerIcon = document.querySelector(".fa-bars");
+const closeIcon = document.querySelector(".fa-xmark");
 navToggle.addEventListener("click", menuFunction);
 primaryNav.addEventListener("click", menuFunction);
 function menuFunction() {
@@ -9,8 +10,12 @@ function menuFunction() {
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", true);
     navToggle.setAttribute("aria-expanded", true);
+    hamburgerIcon.style.display = "none";
+    closeIcon.style.display = "block";
   } else {
     primaryNav.setAttribute("data-visible", false);
     navToggle.setAttribute("aria-expanded", false);
+    closeIcon.style.display = "none";
+    hamburgerIcon.style.display = "block";
   }
 }

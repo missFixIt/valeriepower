1bbb8e2ba6aec84bc0eddc59a6b8dce0e1415e89
missFixIt <126031655+missFixIt@users.noreply.github.com>
2024-02-06 21:45:45 -0800

    updated menu, styling, and expand text functions

--- scripts.js
+++ scripts.js
@@ -2,11 +2,9 @@ const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const menuIcon = document.querySelector(".fa-solid");
 const closeIcon = document.querySelector(".fa-xmark");
-const seeMore = document.querySelector(".see-more");
 
 navToggle.addEventListener("click", menuFunction);
 primaryNav.addEventListener("click", menuFunction);
-seeMore.addEventListener("click", expandFunction);
 
 function menuFunction() {
   const visibility = primaryNav.getAttribute("data-visible");
@@ -23,18 +21,17 @@ function menuFunction() {
   }
 }
 
-function expandFunction() {
-  const expanded = seeMore.getAttribute("expanded");
-  // console.log(visibility);
-  if (expanded === "false") {
-    primaryNav.setAttribute("data-expanded", true);
-    navToggle.setAttribute("aria-expanded", true);
-    seeMore.className = "fa-solid fa-chevron-down see-more";
-  } else {
-    primaryNav.setAttribute("data-expanded", false);
-    navToggle.setAttribute("aria-expanded", false);
-    seeMore.className = "fa-solid fa-chevron-up see-more";
-    // console.log(menuIcon.className);
-    //write something here to remove the cut off from the text so it expands and takes up the whole screen with all the text and photos.
-  }
+// const paraGraphs = document.getElementsByClassName("paragraphs");
+const seeMore = document.getElementsByClassName("see-more");
+
+var i;
+for (i = 0; i < seeMore.length; i++) {
+  seeMore[i].addEventListener("click", function () {
+    this.previousElementSibling.classList.toggle("expanded");
+    if (this.classList.contains("fa-chevron-down")) {
+      this.classList.replace("fa-chevron-down", "fa-chevron-up");
+    } else {
+      this.classList.replace("fa-chevron-up", "fa-chevron-down");
+    }
+  });
 }
